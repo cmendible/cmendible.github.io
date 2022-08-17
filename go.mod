519b26bee7bf01e14239918711f837ab8d52c91e
cmendible <266546+cmendible@users.noreply.github.com>
2022-08-16 21:25:02 -0400

    using hugo modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cmendible/cmendible.github.io
+
+go 1.19
+
+require github.com/razonyang/hugo-theme-bootstrap v0.80.3 // indirect
--- themes/hugo-theme-bootstrap/assets/js/bootstrap.template.js
+++
@@ -1,7 +0,0 @@
-const items = ['mode', 'palette'];
-items.forEach(function (key) {
-const value = localStorage.getItem('hbs-' + key);
-  if (value) {
-    document.body.parentElement.setAttribute('data-' + key, value);
-  }
-});
